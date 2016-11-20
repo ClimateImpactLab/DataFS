667dc45831835cf143601adf525d6ca3d06600a9
Michael Delgado <delgado.michaelt@gmail.com>
2016-11-19 16:50:53 -0800

    add test covering example/local.py

--- examples/local.py
+++ examples/local.py
@@ -24,27 +24,30 @@ from datafs import DataAPI
 from datafs.managers.mongo import MongoDBManager
 from datafs.services.service_os import OSService
 
-MyDataAPI = DataAPI
-MyDataAPI.Manager = MongoDBManager
-MyDataAPI.Services['local'] = OSService
+def main():
+    MyDataAPI = DataAPI
+    MyDataAPI.Manager = MongoDBManager
+    MyDataAPI.Services['local'] = OSService
 
-api = MyDataAPI(
-    username='My Name',
-    contact = 'my.email@example.com')
-
-
-# Create an archive
-archive_name = 'myproject.myteam.var1.type1'
-api.create_archive(
-    archive_name, 
-    description='My test data archive')
-print('created archive "{}"'.format(archive_name))
+    api = MyDataAPI(
+        username='My Name',
+        contact = 'my.email@example.com')
 
 
-# Retrieve the archive
-var1 = api.get_archive(archive_name)
-print('retrieved archive "{}"'.format(var1.archive_name))
+    # Create an archive
+    archive_name = 'myproject.myteam.var1.type1'
+    api.create_archive(
+        archive_name, 
+        description='My test data archive')
+    print('created archive "{}"'.format(archive_name))
 
-# Print metadata from the archive
-print(var1.metadata)
 
+    # Retrieve the archive
+    var1 = api.get_archive(archive_name)
+    print('retrieved archive "{}"'.format(var1.archive_name))
+
+    # Print metadata from the archive
+    print(var1.metadata)
+
+if __name__ == '__main__':
+    main()

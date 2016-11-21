055f16eb1130b6ad2c0c4704d0c5ee221b982868
Michael Delgado <delgado.michaelt@gmail.com>
2016-11-20 18:13:37 -0800

    Bump version: 1.0.0 → 2.0.0

--- datafs/__init__.py
+++ datafs/__init__.py
@@ -7,7 +7,7 @@ from __future__ import absolute_import
 
 __author__ = """Climate Impact Lab"""
 __email__ = 'jsimcock@rhg.com'
-__version__ = '1.0.0'
+__version__ = '2.0.0'
 
 
 from datafs.core.data_api import DataAPI

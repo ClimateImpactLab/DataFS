e5cd05f73266e64e9deb6b03a20a8fda8acd72b9
jgerardsimcock <justin_simcock@yahoo.com>
2016-12-08 13:53:53 -0800

    Bump version: 0.1.2 → 0.1.3

--- datafs/__init__.py
+++ datafs/__init__.py
@@ -7,7 +7,7 @@ from datafs.core.data_api import DataAPI
 
 __author__ = """Climate Impact Lab"""
 __email__ = 'jsimcock@rhg.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 _module_imports = (
     DataAPI,
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ test_requirements = [
 
 setup(
     name='datafs',
-    version='0.1.2',
+    version='0.1.3',
     description="DataFS is an abstraction layer for data storage systems. It manages file versions and metadata using a json-like storage system like AWS's DynamoDB and relies on PyFilesystem to abstract file storage, allowing you to store files locally and on the cloud in a seamless interface.",
     long_description=readme + '\n\n' + history,
     author="Climate Impact Lab",

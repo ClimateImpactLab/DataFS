99898fe838355d6baa2a6aa5b578e2ae34640b79
jgerardsimcock <justin_simcock@yahoo.com>
2016-12-13 09:00:07 -0800

    add datafs

--- datafs/datafs.py
+++ datafs/datafs.py
@@ -4,21 +4,28 @@ import argparse
 from datafs.core.data_api import DataAPI
 
 
+CACHE='path/to/cache/dir'
+
+
 def _download(args):
-	DataAPI.download(args)
+	#DataAPI.download(args)
+	pass
 
 def _upload(args):
-	DataAPI.upload(args)
+	#DataAPI.upload(args)
+	pass
 
 def _create(args):
-	DataAPI.create_archive(args)
+	#DataAPI.create_archive(args)
+	pass
 
 def _list_archvies(args):
-	DataAPI.archives(args)
+	#DataAPI.archives(args)
+	pass
 
 def _metadata(args):
-	DataAPI.managers.get_metadata(args)
-
+	#DataAPI.managers.get_metadata(args)
+	pass
 
 def main(sysArgs=None):
 	parser = argparse.ArgumentParser('Command Line tools for DataFS')
@@ -28,28 +35,29 @@ def main(sysArgs=None):
 
 	#specify the list of arguments we need
 	download = actions.add_parser('download', help='Download an archive')
-	download.add_argument('--archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
+	download.add_argument('archive_name',default=None, help='Name of Archive/data_file')
+	download.add_argument('download_path', help='Path to your default cache directory')
 	#some arguments to add
 	#
 	download.set_defaults(func=_download)
 
 	#another action
 	upload= actions.add_parser('upload', help='Upload an archive')
-	upload.add_argument('--archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
+	upload.add_argument('archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
 	#some more arguments
 	#
 	upload.set_defaults(func=_upload)
 
 	#another acttion
 	create = actions.add_parser('create', help='Create an archive')
-	create.add_argument('--archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
+	create.add_argument('archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
 	#some more required arguments
 	#
 	create.set_defaults(func=_create)
 
 	#another action
 	metadata = actions.add_parser('metadata', help='view metadata for an archive')
-	metadata.add_argument('--archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
+	metadata.add_argument('archive_name', required=True, nargs=1,default=None, help='Name of Archive/data_file')
 
 
 	metadata.set_defaults(fun=_metadata)
@@ -62,11 +70,10 @@ def main(sysArgs=None):
 
 
 	if sysargs is None:
-	        args = parser.parse_args()
-	    else:
-	        args = parser.parse_args(sysargs)
-
-	    args.func(args)
+        args = parser.parse_args()
+	else:
+		args = parser.parse_args(sysargs)
+		args.func(args)
 
 
 

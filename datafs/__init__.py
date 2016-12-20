925aba2b698f2872a7411589e29b2c5eb6aad059
Michael Delgado <mdelgado@rhg.com>
2016-12-19 16:04:13 -0800

    Bump version: 0.4.0 → 0.4.1

--- datafs/__init__.py
+++ datafs/__init__.py
@@ -8,7 +8,7 @@ from datafs.config.config_file import ConfigFile
 from datafs.config.helpers import get_api
 
 __author__ = """Climate Impact Lab"""
-__version__ = '0.4.0'
+__version__ = '0.4.1'
 
 _module_imports = (
     DataAPI,

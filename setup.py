a71311ab64ddbc0bed50c904443f21b688aea783
Michael Delgado <mdelgado@rhg.com>
2017-01-11 14:08:49 -0800

    Bump version: 0.6.2 → 0.6.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ entry_points = '[console_scripts]\ndatafs=datafs.datafs:cli'
 
 setup(
     name='datafs',
-    version='0.6.2',
+    version='0.6.3',
     description="DataFS is an abstraction layer for data storage systems. It manages file versions and metadata using a json-like storage system like AWS's DynamoDB and relies on PyFilesystem to abstract file storage, allowing you to store files locally and on the cloud in a seamless interface.",
     long_description=readme + '\n\n' + history,
     author="Climate Impact Lab",

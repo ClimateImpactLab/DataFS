a9b0ff672c5b54aef7af100c888a3b226f168fbd
Michael Delgado <mdelgado@rhg.com>
2017-02-01 20:45:54 -0800

    Bump version: 0.6.7 → 0.6.8

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ entry_points = '[console_scripts]\ndatafs=datafs.datafs:cli'
 
 setup(
     name='datafs',
-    version='0.6.7',
+    version='0.6.8',
     description=description,
     long_description=readme + '\n\n' + history,
     author="Climate Impact Lab",

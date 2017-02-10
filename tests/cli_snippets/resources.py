47d19e37d168405070872022151aa06c0ce79990
Michael Delgado <delgado.michaelt@gmail.com>
2017-02-10 01:53:43 -0800

    implement multi-test doctests for clis, fix bugs in test_cli_dependencies

--- tests/cli_snippets/resources.py
+++ tests/cli_snippets/resources.py
@@ -1,40 +1,95 @@
 from datafs.datafs import cli
 import shlex
 import re
+import traceback
 
 
 def get_command(string):
-    parsed = re.search(
-        r'\ *\$ (?P<cmd>datafs[^\n]+)(?P<response>(\n\ +[^\n]+)*)',
-        string)
+    r"""
+    Example
+    -------
 
-    command = shlex.split(parsed.group('cmd'))[1:]
-    response = '\n'.join(map(
-        lambda s: s.strip(),
-        parsed.group('response').strip().split('\n')))
+    .. code-block:: python
 
-    lines = response.split('\n')
-    if lines[0] == 'Traceback (most recent call last):':
-        response = None
-        exception = ' '.join(map(lambda s: s.strip(), lines[2:]))
-    else:
-        exception = None
+        >>> test = '''
+        ... .. code-block:: bash
+        ... 
+        ...     $ echo "my string" 
+        ...     ['echo', 'my string']
+        ...
+        ... '''
+        >>> next(get_command(test))
+        (['echo', 'my string'], "['echo', 'my string']", None)
 
-    return command, response, exception
+    .. code-block:: python
+
+        >>> test = '''
+        ...
+        ... .. code-block:: bash
+        ... 
+        ...     $ pytest test_nonexistant.py --cov=datafs --cov=examples \
+        ...     >   --cov=docs --doctest-modules --cov-report term-missing
+        ...     Traceback (most recent call last):
+        ...     ...
+        ...     ERROR: file not found: test_nonexistant.py
+        ...
+        ... '''
+        >>> next(get_command(test))
+        (['pytest', 'test_nonexistant.py', '--cov=datafs', '--cov=examples', '--cov=docs', '--doctest-modules', '--cov-report', 'term-missing'], None, 'ERROR: file not found: test_nonexistant.py')
+
+
+
+    """
+
+    for parsed in re.finditer(
+            (r'(?P<cmd>\$\s+[^\n]+(\\\s*\n\s+\>\ +[^\n]+)*)'
+            +r'(?P<res>(\n\ +[^\$\>\s][^\n]*)*)?'),
+            string):
+
+        cmd = parsed.group('cmd').split('\n')
+
+        formatted = map(
+            lambda s: re.sub(r'\s*[\$\>]\s*', ' ', s.strip()).strip(), cmd)
+
+        command = shlex.split(' '.join(formatted))
+
+        response = '\n'.join(map(
+            lambda s: s.strip(),
+            parsed.group('res').strip().split('\n')))
+
+        lines = response.split('\n')
+        if lines[0] == 'Traceback (most recent call last):':
+            response = None
+            exception = ' '.join(map(lambda s: s.strip(), lines[2:]))
+        else:
+            exception = None
+
+        yield command, response, exception
 
 
 def validate_command(config, command, error=False):
 
     runner, api, config_file, prefix = config
 
-    command, response, exception = get_command(command)
+    for command, response, exception in get_command(command):
 
-    result = runner.invoke(cli, prefix + command)
+        if (len(command) == 0) or (command[0] != 'datafs'):
+            continue
 
-    if error:
-        assert result.exit_code != 0
-        fmt = result.exc_info[0].__name__ + ': ' + str(result.exc_info[1])
-        assert exception == fmt
-    else:
-        assert result.exit_code == 0
-        assert result.output.strip() == response
+        result = runner.invoke(cli, prefix + command[1:])
+
+        if error:
+            assert result.exit_code != 0, result.output
+            fmt = result.exc_info[0].__name__ + ': ' + str(result.exc_info[1])
+            assert exception == fmt
+        else:
+            msg = '\n'+'\n'.join(traceback.format_exception(*result.exc_info))
+            if result.exit_code != 0:
+                raise ValueError("Errors encountered in execution: {}".format(msg))
+
+            msg = '{0} != {1}\n\n+ {0}\n- {1}'.format(result.output, response)
+            assert result.output.strip() == response, msg
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
---
+++ tests/cli_snippets/test_cli_dependencies.py
@@ -0,0 +1,137 @@
+
+from tests.cli_snippets.resources import validate_command
+import pytest
+import os
+
+
+@pytest.mark.cli_snippets
+def test_cli_snippet_1(cli_setup):
+
+    runner, api, config_file, prefix = cli_setup
+
+    with open('arch.txt', 'w+') as f:
+        f.write('contents depend on archive 2 v1.1')
+
+    validate_command(cli_setup, '''
+
+.. EXAMPLE-BLOCK-1-START
+
+.. code-block:: bash
+
+    $ datafs create my_archive
+    created versioned archive <DataArchive local://my_archive>
+
+    $ echo "contents depend on archive 2 v1.1" >> arch.txt
+    
+    $ datafs update my_archive arch.txt  --dependency "archive2==1.1" --dependency "archive3"
+    uploaded data to <DataArchive local://my_archive>. new version 0.0.1 created.
+
+    $ datafs get_dependencies my_archive
+    archive2==1.1
+    archive3
+
+.. EXAMPLE-BLOCK-1-END
+
+''')
+
+    os.remove('arch.txt')
+
+    # Code snippet 2
+
+    runner, api, config_file, prefix = cli_setup
+
+
+    validate_command(cli_setup, '''
+
+.. EXAMPLE-BLOCK-1-START
+
+.. code-block:: bash
+
+    $ datafs set_dependencies my_archive --dependency archive2==1.2
+
+    $ datafs get_dependencies my_archive
+    archive2==1.2
+
+.. EXAMPLE-BLOCK-1-END
+
+''')
+
+    api.delete_archive('my_archive')
+
+
+
+@pytest.mark.cli_snippets
+def test_cli_snippet_3(cli_setup):
+
+    runner, api, config_file, prefix = cli_setup
+
+    validate_command(cli_setup, '''
+
+.. EXAMPLE-BLOCK-3-START
+
+.. code-block:: bash
+
+    $ datafs create my_archive --description 'my test archive'
+    created versioned archive <DataArchive local://my_archive>
+
+.. EXAMPLE-BLOCK-3-END
+
+''')
+
+    api.delete_archive('my_archive')
+
+
+@pytest.mark.cli_snippets
+def test_cli_snippet_4(cli_setup):
+
+    runner, api, config_file, prefix = cli_setup
+
+    api.manager.set_required_archive_metadata({
+        'description': 'Archive description'})
+
+    validate_command(cli_setup, '''
+
+.. EXAMPLE-BLOCK-4-START
+
+.. code-block:: bash
+
+    $ datafs create my_archive --doi '10.1038/nature15725' --author "burke"
+    Traceback (most recent call last):
+    ...
+    AssertionError: Required value "description" not found. Use helper=True or
+    the --helper flag for assistance.
+
+.. EXAMPLE-BLOCK-4-END
+
+''', error=True)
+
+    api.manager.set_required_archive_metadata({})
+
+
+@pytest.mark.cli_snippets
+def test_cli_snippet_5(cli_setup, monkeypatch):
+
+    runner, api, config_file, prefix = cli_setup
+    api.manager.set_required_archive_metadata({
+        'description': 'Enter a description'})
+
+    def get_description(*args, **kwargs):
+        return "my_description"
+
+    # override click.prompt
+    monkeypatch.setattr('click.prompt', get_description)
+
+    validate_command(cli_setup, '''
+
+.. EXAMPLE-BLOCK-5-START
+
+.. code-block:: bash
+
+    $ datafs create my_archive --helper
+    created versioned archive <DataArchive local://my_archive>
+
+.. EXAMPLE-BLOCK-5-END
+
+''')
+
+    api.delete_archive('my_archive')

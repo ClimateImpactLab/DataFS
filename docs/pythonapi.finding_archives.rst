2e4e46d082cd0253afa50bda0b2d18605b51d5a7
jgerardsimcock <justin_simcock@yahoo.com>
2017-03-10 17:21:47 -0800

    add filter docs

--- docs/pythonapi.finding_archives.rst
+++ docs/pythonapi.finding_archives.rst
@@ -6,21 +6,73 @@ Searching and Finding Archives
 ==============================
 
 
+DataFS allows you to search and locate archives with the following methods: 
+:py:meth:`datafs.DataAPI.listdir`, :py:meth:`datafs.DataAPI.filter`, :py:meth:`datafs.DataAPI.search`. Let's look at each method to see how they work. 
+
+Using `listdir`
+---------------
+
+:py:meth:`datafs.DataAPI.listdir` works just like typlical unix style `ls` in the sense that it returns all objects subordinate to the specified directory.
+
+For example if we provide `impactlab/conflict/global` as an argument to `listdir` we get the following. 
+
 .. include:: ../examples/snippets/pythonapi_finding_archives.py
     :start-after: .. EXAMPLE-BLOCK-1-START
     :end-before: .. EXAMPLE-BLOCK-1-END
 
+If your team has used `/` to organize archive naming then you can explore the archive namespace just as you would explore a directory in a filesystem. Let's see what kind of archives we have in our system. 
 
 .. include:: ../examples/snippets/pythonapi_finding_archives.py
     :start-after: .. EXAMPLE-BLOCK-2-START
     :end-before: .. EXAMPLE-BLOCK-2-END
 
+Then if we use `impactlab` as an argument we see that we have several groupings below this. 
 
 .. include:: ../examples/snippets/pythonapi_finding_archives.py
     :start-after: .. EXAMPLE-BLOCK-3-START
     :end-before: .. EXAMPLE-BLOCK-3-END
 
+Let's explore `conflict` to see what kind of archives we have in there. 
 
 .. include:: ../examples/snippets/pythonapi_finding_archives.py
-    :start-after: .. EXAMPLE-BLOCK-4-START
-    :end-before: .. EXAMPLE-BLOCK-4-END
+	:start-after: .. EXAMPLE-BLOCK-4-START
+	:end-before: .. EXAMPLE-BLOCK-4-END
+
+OK. Just one. Now let's have a look at one is inside the `impactlab/conflict/global` namespace. 
+
+.. include:: ../examples/snippets/pythonapi_finding_archives.py
+    :start-after: .. EXAMPLE-BLOCK-5-START
+    :end-before: .. EXAMPLE-BLOCK-5-END
+
+
+ Using `filter`
+ --------------
+
+DataFS also lets you filter so you can limit the search space on archive names. With :py:meth:`datafs.DataAPI.filter` you can use the `prefix`, `path`, `str`, and `regex` pattern options to filter archives.
+Let's look at using the prefix `project1_variable1_` which corresponds to the beginning of a set of archive names. Let's also see how many archives we have in total by filtering without arguments. We see there are 125. By filtering with our prefix we can significantly reduce the number of archives we are looking at. 
+
+.. include:: ../examples/snippets/pythonapi_finding_archives.py
+    :start-after: .. EXAMPLE-BLOCK-6-START
+    :end-before: .. EXAMPLE-BLOCK-6-END
+
+We can also filter on `path`. In this case we want to filter all NetCDF files that match a specific pattern. We need to set our `engine` value to `path` and put in our search pattern. 
+
+.. include:: ../examples/snippets/pythonapi_finding_archives.py
+    :start-after: .. EXAMPLE-BLOCK-7-START
+    :end-before: .. EXAMPLE-BLOCK-7-END
+
+
+We can also filter archives with archive names containing a specific string by setting `engine` to `str`. In this
+example we want all archives with the string `variable2`. The filtering query returns 25 items. Let's look at the first few. 
+
+.. include:: ../examples/snippets/pythonapi_finding_archives.py
+    :start-after: .. EXAMPLE-BLOCK-8-START
+    :end-before: .. EXAMPLE-BLOCK-8-END
+
+
+Using `search`
+--------------
+
+
+DataFS `search` capabilites are enabled via tagging of archives. The arguments of the `search` method are tags associated with a given archive. Archives can be tagged. If archives are not tagged, they cannot be searched with the `search` method. 
+
--- examples/snippets/pythonapi_finding_archives.py
+++ examples/snippets/pythonapi_finding_archives.py
@@ -17,6 +17,7 @@ Setup
     >>> import datafs
     >>> from fs.tempfs import TempFS
     >>> import os
+    >>> import itertools
 
 We test with the following setup:
 
@@ -71,7 +72,6 @@ Set up some archives to search
     >>> socio2 = api.create('impactlab/labor/global/labor_global_daily.csv')
     >>> socio2.update('test.txt')
 
-
 Example 1
 ---------
 
@@ -81,8 +81,8 @@ Displayed example 1 code
 
 .. code-block:: python
 	
-	>>> api.listdir('')
-	[u'impactlab', u'my_archive', u'sample_archive', u'streaming_archive']
+	>>> api.listdir('impactlab/conflict/global')
+	[u'conflict_global_daily.csv']
 
 .. EXAMPLE-BLOCK-1-END
 
@@ -95,9 +95,9 @@ Displayed example 2 code
 
 .. code-block:: python
 	
-	>>> api.listdir('impactlab')
-	[u'labor', u'climate', u'conflict', u'mortality']
-	
+	>>> api.listdir('')
+	[u'impactlab']
+
 .. EXAMPLE-BLOCK-2-END
 
 Example 3
@@ -109,9 +109,9 @@ Displayed example 3 code
 
 .. code-block:: python
 	
-	>>> api.listdir('impactlab/conflict')
-	[u'global']
-
+	>>> api.listdir('impactlab')
+	[u'labor', u'climate', u'conflict', u'mortality']
+	
 .. EXAMPLE-BLOCK-3-END
 
 Example 4
@@ -123,13 +123,26 @@ Displayed example 4 code
 
 .. code-block:: python
 	
+	>>> api.listdir('impactlab/conflict')
+	[u'global']
+
+.. EXAMPLE-BLOCK-4-END
+
+Example 5
+---------
+
+Displayed example 5 code
+
+.. EXAMPLE-BLOCK-5-START
+
+.. code-block:: python
+	
 	>>> api.listdir('impactlab/conflict/global')
 	[u'conflict_global_daily.csv']
 	>>> api.listdir('impactlab/conflict/global/conflict_global_daily.csv')
 	[u'0.0.1']
 
-.. EXAMPLE-BLOCK-4-END
-
+.. EXAMPLE-BLOCK-5-END
 
 Teardown
 --------
@@ -146,6 +159,129 @@ Teardown
     ... except KeyError:
     ...     pass
     ...
+
+
+
+Filter example setup
+
+.. code-block:: python
+
+	>>> archive_names = []
+    >>> for indices in itertools.product(*(range(1, 6) for _ in range(3))):
+    ...		archive_name = ('project{}_variable{}_scenario{}.nc'.format(*indices))
+    ... 	archive_names.append(archive_name)
+    >>> for name in archive_names:
+    ...		_ = api.create(name)
+	
+
+
+
+
+
+Example 6
+---------
+
+Displayed example 6 code
+
+.. EXAMPLE-BLOCK-6-START
+
+.. code-block:: python
+>>> len(list(api.filter()))
+125
+>>> v1 = api.filter(prefix='project1_variable1_')
+>>> list(v1)
+[u'project1_variable1_scenario1.nc', u'project1_variable1_scenario2.nc', u'project1_variable1_scenario3.nc', u'project1_variable1_scenario4.nc', u'project1_variable1_scenario5.nc']
+	
+	
+.. EXAMPLE-BLOCK-6-END
+
+
+Example 7
+---------
+
+Displayed example 7 code
+
+.. EXAMPLE-BLOCK-7-START
+
+.. code-block:: python
+
+>>> v2 = api.filter(pattern='*_variable4_scenario4.nc', engine='path')
+>>> list(v2)
+[u'project1_variable4_scenario4.nc', u'project2_variable4_scenario4.nc', u'project3_variable4_scenario4.nc', u'project4_variable4_scenario4.nc', u'project5_variable4_scenario4.nc']
+
+
+.. EXAMPLE-BLOCK-7-END
+
+
+Example 8
+---------
+
+Displayed example 8 code
+
+.. EXAMPLE-BLOCK-8-START
+
+.. code-block:: python
+
+>>> v3 = list(api.filter(pattern='variable2', engine='str'))
+>>> len(v3)
+25
+>>> v3[:4]
+[u'project1_variable2_scenario1.nc', u'project1_variable2_scenario2.nc', u'project1_variable2_scenario3.nc', u'project1_variable2_scenario4.nc']
+
+
+.. EXAMPLE-BLOCK-8-END
+
+
+Example 9
+---------
+
+Displayed example 9 code
+
+.. EXAMPLE-BLOCK-9-START
+
+.. code-block:: python
+
+
+
+
+.. EXAMPLE-BLOCK-9-END
+
+
+Example 10
+---------
+
+Displayed example 10 code
+
+.. EXAMPLE-BLOCK-10-START
+
+.. code-block:: python
+
+	>>> archives_search = list(api.search())
+	>>> archives_filter = list(api.filter())
+	>>> len(archives_search)
+	125
+	>>> len(archives_filter)
+	125
+.. EXAMPLE-BLOCK-10-END
+
+
+Example 11
+---------
+
+Displayed example 11 code
+
+.. EXAMPLE-BLOCK-11-START
+
+.. code-block:: python
+
+
+
+
+.. EXAMPLE-BLOCK-11-END
+
+
+
+
 
     >>> api.manager.delete_table('DataFiles')
 
